07da68b8c6c052b83b660f59eecad37a8c1ed73c
Kevin <Requenak970@gmail.com>
2026-06-13 16:39:33 -0500

    dos roles nuevos y pantalla qr

--- database/migrations/2026_06_13_162900_insert_soporte_and_admin_general_roles.php
+++ database/migrations/2026_06_13_162900_insert_soporte_and_admin_general_roles.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         DB::table('roles')->insert([
@@ -16,9 +13,6 @@ return new class extends Migration
         ]);
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         DB::table('roles')->whereIn('nombre', ['SOPORTE', 'ADMIN_GENERAL'])->delete();
